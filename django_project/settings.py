856712d3b406dc36c08335b3267b62eddf5a01cb
tugrulp <tugrul.pinar@proton.me>
2024-04-07 22:38:42 -0400

    change static

--- django_project/settings.py
+++ django_project/settings.py
@@ -34,7 +34,7 @@ INSTALLED_APPS = [
     "allauth",
     "allauth.account",
     "crispy_forms",
-    "crispy_bootstrap5",
+    # "crispy_bootstrap5",
     "debug_toolbar",
     "bootstrap_datepicker_plus",
     "bootstrap4",
@@ -130,7 +130,7 @@ STORAGES = {
         "BACKEND": "django.core.files.storage.FileSystemStorage",
     },
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
     },
 }
 
